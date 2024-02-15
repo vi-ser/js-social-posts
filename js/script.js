37206d70adc64bc81869bcfe4652929738fd1d12
vi_ser <vinceulisse@gmail.com>
2024-02-15 15:28:57 +0100

    stampati post nel feed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /*
 Descrizione
 Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro script JS in cui:
+
 Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
 - id del post, numero progressivo da 1 a n
 - nome autore,
@@ -9,9 +10,11 @@ Milestone 1 - Creiamo il nostro array di oggetti che rappresentano ciascun post.
 - testo del post,
 - immagine (non tutti i post devono avere una immagine),
 - numero di likes.
+
 Non è necessario creare date casuali Per le immagini va bene utilizzare qualsiasi servizio di placeholder.
 
 Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
 Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo. Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 
 BONUS
@@ -22,58 +25,103 @@ BONUS
 
 const posts = [
     {
-        "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
+        id: 1,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/300?image=171",
+        author: {
+            name: "Phil Mangione",
+            image: "https://unsplash.it/300/300?image=15"
         },
-        "likes": 80,
-        "created": "2021-06-25"
+        likes: 80,
+        created: "2021-06-25",
     },
     {
-        "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
+        id: 2,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=112",
+        author: {
+            name: "Sofia Perlari",
+            image: "https://unsplash.it/300/300?image=10"
         },
-        "likes": 120,
-        "created": "2021-09-03"
+        likes: 120,
+        created: "2021-09-03",
     },
     {
-        "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
+        id: 3,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=234",
+        author: {
+            name: "Chiara Passaro",
+            image: "https://unsplash.it/300/300?image=20"
         },
-        "likes": 78,
-        "created": "2021-05-15"
+        likes: 78,
+        created: "2021-05-15",
     },
     {
-        "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
-        "author": {
-            "name": "Luca Formicola",
-            "image": null
+        id: 4,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=24",
+        author: {
+            name: "Luca Formicola",
+            image: null
         },
-        "likes": 56,
-        "created": "2021-04-03"
+        likes: 56,
+        created: "2021-04-03",
     },
     {
-        "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
-        "author": {
-            "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
+        id: 5,
+        content: "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        media: "https://unsplash.it/600/400?image=534",
+        author: {
+            name: "Alessandro Sainato",
+            image: "https://unsplash.it/300/300?image=29"
         },
-        "likes": 95,
-        "created": "2021-03-05"
+        likes: 95,
+        created: "2021-03-05",
     }
 ];
+
+const postListElement = document.getElementById("container");
+
+
+posts.forEach(function (currentPost) {
+
+    // prima assegno un'immagine generica a chi non ce l'ha
+    if (currentPost.author.image == null) {
+        currentPost.author.image = "./img/profile-pic.png";
+    }
+
+    // inserisco l'elemento html dentro la lista dei post
+    postListElement.innerHTML += `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${currentPost.author.image}" alt="${currentPost.author.name}">
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${currentPost.author.name}</div>
+                <div class="post-meta__time">${currentPost.created}</div>
+            </div>
+        </div>
+    </div>
+    <div class="post__text">${currentPost.content}</div>
+    <div class="post__image">
+        <img src="${currentPost.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="${currentPost.id}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${currentPost.likes}</b> persone
+            </div>
+        </div>
+    </div>
+</div>
+    `
+})
